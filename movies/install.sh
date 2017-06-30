76b23fb8b655d8f3063e58778da56a9bfddf6f21
Abouzar Parvan <aboozar.parvan@ut.ac.ir>
2017-06-30 19:18:08 +0430

    Update install.sh

--- movies/install.sh
+++ movies/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo -n "Installing movies: "
-brew install googler
+if brew help 1>/dev/null 2>/dev/null; then
+        brew install googler
+fi
 chmod a+x movies
 cp movies /usr/local/bin > /dev/null 2>&1 || { echo "Failure"; echo "Error copying file, try running install script as sudo"; exit 1; }
 echo "Success"
