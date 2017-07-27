48eac84877c40d8e69b3b182c239b6b629cdb20c
Alexey Melezhik <melezhik@gmail.com>
2017-07-27 11:35:55 +0300

    remove outthentic tests

--- outthentic-test/hook.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-declare -a tools=$(config tools)
-
-
-for t in "${tools[@]}"; do
-   run_story $t
-done
-
--- outthentic-test/modules/cheat/story.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'help'
-bash cheat/cheat
-echo
-
-echo example1
-bash cheat/cheat lua hello
-
