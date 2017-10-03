c0ed6fd8a1c13798dc91b4642d19b5b45ec15f80
Reto Kromer <retokromer@users.noreply.github.com>
2017-10-03 03:33:30 +0200

    Spacing fixes (#126)

--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,7 @@ all="1"
 
 askUninstall()
 {
-  if [[ -f  /usr/local/bin/$1 ]]; then
+  if [[ -f /usr/local/bin/$1 ]]; then
     echo -n "Do you wish to uninstall $1 [Y/n]: "
     read -r answer
     if [[ "$answer" == [Yy] ]]; then
@@ -21,7 +21,7 @@ askUninstall()
 
 removeTool()
 {
-  if [[ -f  /usr/local/bin/$1 ]]; then
+  if [[ -f /usr/local/bin/$1 ]]; then
     echo -n "Removing $1: "
     rm -f /usr/local/bin/$1 > /dev/null 2>&1 || { echo "Failed" ; echo "Error removing file, try running uninstall script as sudo"; exit 1; }
     echo "Success"
@@ -34,7 +34,7 @@ removeManpage()
   if [ -f "/usr/local/share/man/man1/bash-snippets.1" ]; then rm -f "/usr/local/share/man/man1/bash-snippets.1" || { echo "Error removing manpage, try running uninstall script as sudo"; exit 1; } ; fi
 }
 
-if [[ $1 != "all" ]];then
+if [[ $1 != "all" ]]; then
   for tool in "${tools[@]}"; do
     askUninstall $tool || exit 1
   done
